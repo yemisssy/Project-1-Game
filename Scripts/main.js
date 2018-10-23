3271e5b3fb4ab18d1013d2628fc7900cf73a32bb
student_105 <student_105@ool-ad02066e.dyn.optonline.net>
2018-10-23 09:12:12 -0400

    pushing to my personal github

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+console.log('hello');
+//Tutorial video div
+let tutorial=document.createElement('div');
+tutorial.id= 'tutVid';
+document.body.appendChild(tutorial)
+//Div holding player's name input
+let inputDiv =document.createElement('div');
+inputDiv.id='inputDiv';
+document.body.appendChild(inputDiv);
